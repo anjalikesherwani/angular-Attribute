addb0400dd3a0dc85bca8e236a16bd10adabca82
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-02-19 13:07:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'bootstrap';
+  uname = 'Anjali';
+  uage = 10;
+  // student = [
+  //   { id: 101, name: 'John Doe', loc:'Ald' ,laptop:'Asus'},
+  //   { id: 102, name: 'Jane Smith',loc:'Ald', laptop:'HP'},
+  //   { id: 203, name: 'Anjali', loc:'Ptn' ,laptop:'Lenovo'},
+  //   { id: 426, name: 'jyoti',loc:'Ddn', laptop:'Dell'},
+  //   { id: 234, name: 'vaishu', loc:'Knp' ,laptop:'Acer'},
+  //   { id: 145, name: 'Ashish',loc:'Ramnp', laptop:'HP'},
+
+  // ]
+
+  // anjali(laptop:any){
+  //   switch(laptop){
+  //     case 'Hp': return "orange";
+  //     case 'Lenovo': return "yellow";
+  //     case 'Asus': return "green";
+  //     case 'Dell': return "blue";
+  //     case 'Acer': return "pink";
+      
+  //   }
+  //   return null;
+  // }
 }
